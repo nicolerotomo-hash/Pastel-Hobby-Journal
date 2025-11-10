24985653f489f86b8a6a578da8c863bd17dc4193
nicolerotomo-hash <nicolerotomo@gmail.com>
2025-11-10 08:32:51 +0800

    Update cache version from v4 to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pastel-hobby-v4";
+const CACHE = "pastel-hobby-v5";
 const ASSETS = ["./","./index.html","./app.js","./manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
